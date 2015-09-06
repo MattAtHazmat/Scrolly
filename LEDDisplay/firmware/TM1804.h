9890cff3dd6ff0170a648d4d60cef1d4e7c63c52
Matt Bennett <matt.bennett@gmail.com>
2015-09-06 06:58:36 -0500

    A bit of reformatting

--- LEDDisplay/firmware/TM1804.h
+++ LEDDisplay/firmware/TM1804.h
@@ -35,21 +35,13 @@
 #ifndef TM1804_H
 #define    TM1804_H
 
-#define LED_STRIP_ZERO  0b11100000
-#define LED_STRIP_ONE   0b11111000
-#define LED_STRIP_RESET_BITS   (24)
+#define LED_STRIP_ZERO          (0b11100000)
+#define LED_STRIP_ONE           (0b11111000)
+#define LED_STRIP_RESET_BITS    (24)
 
-#define PIXEL_COLOR_0 red
-#define PIXEL_COLOR_1 green
-#define PIXEL_COLOR_2 blue
+#define PIXEL_COLOR_0           red
+#define PIXEL_COLOR_1           green
+#define PIXEL_COLOR_2           blue
 
-//#define ORDER_RED   (0)
-//#define ORDER_GREEN (1)
-//#define ORDER_BLUE  (2)
-
-/* for WS2812 */
-//#define ORDER_RED   (1)
-//#define ORDER_GREEN (0)
-//#define ORDER_BLUE  (2)
 #endif    /* TM1804_H */
 
