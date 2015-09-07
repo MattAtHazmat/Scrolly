4f21e067e708fd2c754ba1ba0ac903c595399542
Matt Bennett <matt.bennett@gmail.com>
2015-09-07 08:59:18 -0500

    Imported The Wifi G demo app- the files that are not included in Harmony- those that are created locally.

---
+++ ScrollyWiFi.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# wifi_g_db configuration
+CND_ARTIFACT_DIR_wifi_g_db=dist/wifi_g_db/production
+CND_ARTIFACT_NAME_wifi_g_db=ScrollyWiFi.X.production.hex
+CND_ARTIFACT_PATH_wifi_g_db=dist/wifi_g_db/production/ScrollyWiFi.X.production.hex
+CND_PACKAGE_DIR_wifi_g_db=${CND_DISTDIR}/wifi_g_db/package
+CND_PACKAGE_NAME_wifi_g_db=scrollywifi.x.tar
+CND_PACKAGE_PATH_wifi_g_db=${CND_DISTDIR}/wifi_g_db/package/scrollywifi.x.tar
